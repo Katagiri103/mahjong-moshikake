73ac615d74b04a741d29c1fcfa2a79a558b0806f
compliment <kensuke30606@gmail.com>
2022-06-24 23:39:16 +0900

    ２～4位のスコア処理は完成。

--- mahjong-moshikake/app/Http/Controllers/MainController.php
+++ mahjong-moshikake/app/Http/Controllers/MainController.php
@@ -22,4 +22,28 @@ class MainController extends Controller
         ];
         return view('main.table', $data);
     }
+
+    public function finish(Request $request){
+        $score = $request->score;
+        $rank = $request->rank;
+        if($rank == 1){ //1位の場合の点数処理
+            
+        }elseif($rank == 2){ //2位の場合の点数処理
+            $score2 = $score - 30000;
+            $score3 = round($score2 / 1000);
+            $trueScore = $score3 + 10;
+
+        }elseif($$rank == 3){ //3位の場合の点数処理
+            $score2 = $score - 30000;
+            $score3 = round($score2 / 1000);
+            $trueScore = $score3 - 10;
+            
+        }else{ //4位の場合の点数処理
+            $score2 = $score - 30000;
+            $score3 = round($score2 / 1000);
+            $trueScore = $score3 - 20;
+        };
+        return view('main.table', $trueScore);
+        
+    }
 }
